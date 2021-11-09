3da8debb85273241c08c56d4c59e1b62668bf0ca
Mahesh Rayas <maheshrayas@gmail.com>
2021-11-09 21:28:43 +1100

    support both pr and on run on main branch

--- src/git.rs
+++ src/git.rs
@@ -1,10 +1,11 @@
 use anyhow::{Context, Result};
 use regex::Regex;
 use std::fs;
+use std::env;
 
 use git2::{
-    Commit, Cred, Direction, IndexAddOption, ObjectType, PushOptions, RemoteCallbacks, Repository,
-    Signature,
+    Cred, Direction, IndexAddOption, ProxyOptions, PushOptions, RemoteCallbacks, Repository,
+    Signature,Oid,
 };
 
 const RED: &str = "red";
@@ -17,6 +18,8 @@ pub(crate) struct Git<'a> {
    pub(crate) http_user: &'a str,
    pub(crate) http_pass: &'a str,
    pub(crate) git_email: &'a str,
+   pub(crate) proxy: &'a str,
+   pub(crate) test_on: &'a str,
 }
 
 pub(crate) fn get_color<'a>(percentage: f64) -> &'a str {
@@ -32,12 +35,14 @@ pub(crate) fn get_color<'a>(percentage: f64) -> &'a str {
 }
 
 impl<'a> Git<'a> {
-   pub(crate) fn new(branch: &'a str, http_user:&'a str, http_pass:&'a str,git_email:&'a str) -> Self {
+   pub(crate) fn new(branch: &'a str, http_user:&'a str, http_pass:&'a str,git_email:&'a str, proxy:&'a str,test_on:&'a str) -> Self {
         Git{
         branch,
         http_user,
         http_pass,
         git_email,
+        proxy,
+        test_on,
        }
    }
 
@@ -77,16 +82,23 @@ impl<'a> Git<'a> {
         Ok(())
     }
 
-    pub(crate) fn commit_push(&self
+    pub(crate) fn commit_push_to_default_branch(&self
     ) -> Result<()> {
         let repo = Repository::open(".")
             .with_context(|| "Something went wrong while setting up repository".to_string())?;
         let mut index = repo.index()?;
         index.add_all(["README.md"].iter(), IndexAddOption::FORCE, None)?;
         index.write()?;
-        let oid = index.write_tree()?;
+
+        let oid : Oid;
+        if self.test_on == "default"{
+             oid = index.write_tree()?;
+        }else{
+            let git_ref= env::var("GITHUB_HEAD_REF").unwrap();
+            oid = Oid::from_str(git_ref.as_str()).unwrap();
+        }
         let signature = Signature::now(self.http_user, self.git_email)?;
-        let parent_commit = Self::find_last_commit(&repo)?;
+        let parent_commit = repo.find_commit(oid).unwrap();
         let tree = repo.find_tree(oid)?;
         repo.commit(
             Some("HEAD"),
@@ -97,6 +109,11 @@ impl<'a> Git<'a> {
             &[&parent_commit],
         )?;
         let mut remote = repo.find_remote("origin")?;
+
+        let mut p = ProxyOptions::new();
+        if self.proxy != "" {
+            p.url(&self.proxy);
+        }
 
         remote.connect_auth(
             Direction::Push,
@@ -116,19 +133,62 @@ impl<'a> Git<'a> {
             .join("")],
             Some(&mut push_options),
         )?;
-
         Ok(())
     }
 
-    fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
-        let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
-        obj.into_commit()
-            .map_err(|_| git2::Error::from_str("Couldn't find commit"))
-    }
+    // fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
+    //     let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
+    //     obj.into_commit()
+    //         .map_err(|_| git2::Error::from_str("Couldn't find commit"))
+    // }
+
+    // fn commit_push(&self) -> Result<()> {
+    //     let repo = Repository::open(".")
+    //     .with_context(|| "Something went wrong while cloning".to_string())?;
+    //     let mut index = repo.index()?;
+    //     let my_oid_str =  env::var("GITHUB_HEAD_REF").unwrap().as_str();
+    //     let oid = Oid::from_str(my_oid_str).unwrap();
+    //     let commit = repo.find_commit(oid).unwrap();
+    
+    //     let branch = repo.branch(
+    //         my_oid_str,
+    //         &commit,
+    //         false,
+    //     );
+    
+    //     let obj = repo.revparse_single(&("refs/heads/".to_owned() + my_oid_str)).unwrap(); 
+    
+    //     repo.checkout_tree(
+    //         &obj,
+    //         None,
+    //     );
+    //     repo.set_head(&("refs/heads/".to_owned() + my_oid_str));
+    //     let mut remote = repo.find_remote("origin")?;
+    //     remote.connect_auth(
+    //         Direction::Push,
+    //         Some(self.create_callbacks()),
+    //         None,
+    //     )?;
+    //     let mut push_options = PushOptions::default();
+    //     push_options.remote_callbacks(self.create_callbacks());
+
+    //     let ref_specs = format!("refs/heads/{}", self.branch);
+
+    //     remote.push(
+    //         &[[
+    //             String::from("+"),
+    //             [ref_specs.to_owned(), ref_specs].join(":"),
+    //         ]
+    //         .join("")],
+    //         Some(&mut push_options),
+    //     )?;
+    
+    // }
 
     fn create_callbacks(&self) -> RemoteCallbacks {
         let mut callbacks = RemoteCallbacks::new();
         callbacks.credentials(move |_, _, _| Cred::userpass_plaintext(self.http_user, self.http_pass));
         callbacks
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod lang;
 use lang::read_cov_report;
 use git::Git;
 
-const BRANCH: &str = "coverage";
+
 
 fn main() -> Result<()> {
     let env = Env::default()
@@ -23,20 +23,43 @@ fn main() -> Result<()> {
     let token = env::var("INPUT_TOKEN").expect("Missing input parameter: Token");
     let user = env::var("INPUT_USER").expect("Missing input parameter: USER");
     let email = env::var("INPUT_EMAIL").expect("Missing input parameter: EMAIL");
+    let test_on = env::var("INPUT_TEST_ON").expect("Missing input parameter: TEST_ON");
     // set environment variable for gh
     env::set_var("GITHUB_TOKEN", &token);
-    let git = Git::new(BRANCH,&user,&token,&email );
-    git.git_branch()?;
-    info!("Currently on branch {}",BRANCH);
     let percent = read_cov_report()?;
     let badge_color = git::get_color(percent);
+
+    let proxy = match env::var("HTTP_PROXY") {
+        Ok(proxy) => proxy,
+        Err(_) => String::from(""),
+    };
+
+    let branch = if test_on == "default" {
+        "coverage".to_string()
+    }else{
+        env::var("GITHUB_HEAD_REF").unwrap()
+    };
+
+    let git = Git::new(&branch, &user, &token, &email, &proxy,&test_on);
+
     // update the readme
     Git::<'_>::update_readme(percent, badge_color, "README.md")?;
-    // commit the code
-    git.commit_push()?;
-    // creat pr
-    gh::create_pr()?;
-    // merge the pr
-    gh::merge_pr()?;
+
+    if test_on == "pr" {
+        // commit the code
+        git.commit_push_to_default_branch()?;
+
+    }else {
+
+        git.git_branch()?;
+        info!("Currently on branch {}",branch);
+        // commit the code
+        git.commit_push_to_default_branch()?;
+        // creat pr
+        gh::create_pr()?;
+        // merge the pr
+        gh::merge_pr()?;
+    }
+
     Ok(())
 }
